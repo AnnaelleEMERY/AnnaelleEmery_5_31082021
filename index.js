abec76faa312098a2b2c0b7389ec71824054efb4
Annaelle <annaelleemery512@gmail.com>
2021-09-08 17:05:22 +0200

    js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // js
-fetch('http://localhost:3000/api/teddies')
-.then(response => response.json())
-.then(data => {
-    const articlesContainer = document.getElementById('articles');
-    for (let article of data) {
-        articlesContainer.innerHTML += article.name;
-    }
-});
+fetch('http://localhost:3000/api/cameras')
+  .then(response => response.json())
+  .then(data => {
+      const articlesContainer = document.getElementById('articles');
+      for (let article of data) {
+          articlesContainer.innerHTML += `${article.name} ${article.price}<br>`;
+      }
+  });
