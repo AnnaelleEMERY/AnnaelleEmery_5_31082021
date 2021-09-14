b9952051b75c3509fc9124b4cf955134e9c2df7f
Annaelle <annaelleemery512@gmail.com>
2021-09-14 11:44:26 +0200

    page boutique terminée

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// Allons chercher les données !!
 fetch(('https://annaelle-orinoco-api.herokuapp.com/api/teddies'))
   .then(response => response.json())
   .then(data => {
+
+    // Choix de l'élément dans lequel nous intégrons ces données
     let teddiesList = document.querySelector('.card');
 
-
+    // Intégration des blocs et des données qui seront dedans
     for (let teddy of data) {
-        let cardElt = document.createElement('article');
+        let cardElt = document.createElement('a');
         let contentElt = document.createElement('div');
         let picElt = document.createElement('img');
         let nameElt = document.createElement('h3');
@@ -15,13 +18,15 @@ fetch(('https://annaelle-orinoco-api.herokuapp.com/api/teddies'))
 
         picElt.src = teddy.imageUrl;
         nameElt.textContent = teddy.name;
-        priceElt.textContent = teddy.price;
+        priceElt.textContent = teddy.price + "€";
         descriptionElt.textContent = teddy.description;
-        btnElt.textContent = "Acheter le produit";
+        btnElt.textContent = "Ajouter au panier";
 
         teddiesList.appendChild(cardElt);
+
         cardElt.appendChild(picElt);
         cardElt.appendChild(contentElt)
+
         contentElt.appendChild(nameElt);
         contentElt.appendChild(priceElt);
         contentElt.appendChild(descriptionElt);
@@ -30,13 +35,11 @@ fetch(('https://annaelle-orinoco-api.herokuapp.com/api/teddies'))
         cardElt.classList.add('teddy-card');
         contentElt.classList.add('teddy-card__content');
         picElt.classList.add('teddy-card__pic');
-        descriptionElt.add('teddy-card__description');
-        priceElt.add('teddy-card__price')
+        descriptionElt.classList.add('teddy-card__description');
+        priceElt.classList.add('teddy-card__price')
         btnElt.classList.add('teddy-card__btn');
 
-        btnElt.setAttribute('href', './pages/product.html?id=' + teddy._id);
+        cardElt.setAttribute('href', './pages/product.html?id=' + teddy._id);
     }
-  });
-
-  
+});
 
