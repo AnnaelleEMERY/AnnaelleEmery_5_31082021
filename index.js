702d29d7ba2878af1f7e8d8550eb8a5ec4a2e7d7
Annaelle <annaelleemery512@gmail.com>
2021-09-29 18:45:59 +0200

    modification commentaires

--- index.js
+++ index.js
@@ -493,7 +493,12 @@ addition = () => {
     console.log('aEnvoyer');
     console.log(aEnvoyer);
 
-    //envoie de l'objet aEnvoyer VERS LE SERVEUR
+
+    
+/**********************************************************************************************************/
+/******************************************Envoi VERS LE SERVEUR*********************************************/
+/**********************************************************************************************************/
+
 
     const promise01 = fetch("https://annaelle-orinoco-api.herokuapp.com/api/teddies/order/", {
       method: "POST",
@@ -555,65 +560,6 @@ addition = () => {
 
 
 
-/**********************************************************************************************************/
-/******************************************Envoi du formulaire*********************************************/
-/**********************************************************************************************************/
-
-
-/*
-//Fonction request post de l'API
-sendData = (objetRequest) => {
-  return new Promise((resolve) => {
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
-        //Sauvegarde du retour de l'API dans la sessionStorage pour affichage dans order-confirm.html
-        sessionStorage.setItem("order", this.responseText);
-
-        //Chargement de la page de confirmation
-        document.forms["form-panier"].action = '/index-confirmation.html';
-        document.forms["form-panier"].submit();
-
-        resolve(JSON.parse(this.responseText));
-      }
-    };
-    request.open("POST", APIURL + "order");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(objetRequest);
-  });
-};
-
-//Au click sur le btn de validation du formulaire
-validForm = () => {
-  //Ecoute de l'event click du formulaire
-  let btnForm = document.getElementById("envoiPost");
-  btnForm.addEventListener("click", function () {
-    //Lancement des verifications du panier et du form => si Ok envoi
-    if (checkCart() == true && checkInput() != null) {
-      console.log("Administration : L'envoi peut etre fait");
-      //Création de l'objet à envoyer
-      let objet = {
-        contact,
-        products
-      };
-      console.log("Administration : " + objet);
-      //Conversion en JSON
-      let objetRequest = JSON.stringify(objet);
-      console.log("Administration : " + objetRequest);
-      //Envoi de l'objet via la function
-      sendData(objetRequest);
-
-      //Une fois la commande faite retour à l'état initial des tableaux/objet/localStorage
-      contact = {};
-      products = [];
-      localStorage.clear();
-    } else {
-      console.log("Administration : ERROR");
-    };
-  });
-};
-
-*/
 
 
 /**********************************************************************************************************/
